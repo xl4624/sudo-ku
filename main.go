12e5b2f930d1ba716e5cdfc381318d608e68fa9d
Xiaomin Liu <xl4624@nyu.edu>
2024-02-15 00:00:20 -0500

    Add tests to measure solving speeds

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+    "time"
 
 	"github.com/xl4624/Sudo-ku/api"
 	"github.com/xl4624/Sudo-ku/pkg"
@@ -11,18 +12,18 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("Please provide a difficulty level: easy, medium, hard, tough, parents")
+		log.Fatal("Please provide a difficulty level: easy, medium, hard, tough, or test")
 	}
 	difficulty := os.Args[1]
 
 	switch difficulty {
-	case "easy", "medium", "hard", "tough", "parents":
+	case "easy", "medium", "hard", "tough":
+    case "test":
+        testSolveTime()
+        return
 	default:
-		log.Fatalf("Invalid difficulty level: %v. Please provide a difficulty level: easy, medium, hard, tough, parents", difficulty)
+		log.Fatalf("Invalid difficulty level: %v. Please provide a difficulty level: easy, medium, hard, tough", difficulty)
 	}
-
-	// handler := api.NewHandler()
-	// http.HandleFunc("/api/solve", handler.SolveHandler)
 
 	fetcher := api.NewPuzzleFetcher()
 	fetcher.SetDifficulty(difficulty)
@@ -41,6 +42,29 @@ func main() {
 		fmt.Println("Solution:")
 		grid.Display()
 	}
+}
 
-	// log.Fatal(http.ListenAndServe(":8000", nil))
+func testSolveTime() {
+    difficulties := []string{"easy", "medium", "hard", "tough"}
+    sampleSize := 5
+
+    for _, difficulty := range difficulties {
+        var totalDuration time.Duration
+        for i := 0; i < sampleSize; i++ {
+            fetcher := api.NewPuzzleFetcher()
+            fetcher.SetDifficulty(difficulty)
+            grid, err := fetcher.FetchPuzzle()
+            if err != nil {
+                log.Fatalf("Failed to fetch puzzle: %v", err)
+            }
+
+            start := time.Now()
+            solver := pkg.NewSolver(&grid)
+            solver.DfsBacktrackSolve()
+            totalDuration += time.Since(start)
+        }
+
+        averageDuration := totalDuration / time.Duration(sampleSize)
+        fmt.Printf("Average time to solve %v puzzle: %v\n", difficulty, averageDuration)
+    }
 }
